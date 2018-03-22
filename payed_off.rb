4ee6dc3136fe0dfd09280c29ca7e6636c2a84d45
Joe Begley <joebegley@Joes-MacBook-Air.local>
2018-03-22 15:19:22 -0400

    Created a working file that asks for your amount owed, time to pay off and interest rate

--- payed_off.rb
+++ payed_off.rb
@@ -3,7 +3,29 @@ def payedOff(balance, period, iRte)
 	min = 0
 	balaceReset = balance
 	payment = ((max + min) / 2).round(2)
+	conditional = true
+	while conditional do
+		balance = balaceReset
+		payment = ((max + min) / 2).round(2)
+		period.times do
+			balance = ((balance - payment) * (1 + (iRte) / 12)).round(2)
+		end
+		if balance > -(0.5) and balance < 0
+			conditional = false
+		elsif balance < 0
+			max = payment
+		else
+			min = payment
+		end
+	end
 	return payment
 end
 
-puts payedOff(17000, 48, 1)
+puts "How much do you owe?"
+thisBalance = gets.strip.to_i
+puts "How long do you have to pay it off?"
+thisMonths = gets.strip.to_i
+puts "What is your interest rate?"
+thisRate = gets.strip.to_i
+
+puts payedOff(thisBalance, thisMonths, thisRate)
